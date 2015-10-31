496c430d5e62dd23231c41e702b5c52cd4acd4cc
LasmGratel <lasm_gratel@hotmail.com>
2015-10-31 18:28:16 +0800

    add commmand & format

--- src/main/java/org/infinitystudio/singularity/api/SingularityRegistery.java
+++ src/main/java/org/infinitystudio/singularity/api/SingularityRegistery.java
@@ -1,19 +1,18 @@
 /**
- * Singularity Mod for Minecraft.
- * Copyright (C) 2015 Infinity Studio.
+ * Singularity Mod for Minecraft. Copyright (C) 2015 Infinity Studio.
  * <p/>
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  * <p/>
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  * <p/>
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * @license GPLv3
  */
@@ -33,27 +32,34 @@ import org.infinitystudio.singularity.item.SingularityItem;
 
 /**
  * The Singularity Mod's Registery.
+ * 
  * @author Darkhighness
  *
  */
-public class SingularityRegistery extends GameRegistry {
+public class SingularityRegistery extends GameRegistry
+{
 
     /**
      * To register an ingot with OreDictionary.
-     * @param Ingot The object to be registered.
+     * 
+     * @param Ingot
+     *            The object to be registered.
      */
-    public static void registerIngotWithOreDictionary(SingularityItem Ingot) {
-        OreDictionary.registerOre(Ingot.getName(), Ingot);
+    public static void registerIngotWithOreDictionary(SingularityItem Ingot)
+    {
+	OreDictionary.registerOre(Ingot.getName(), Ingot);
     }
 
     /**
      * To register an block with OreDictionary.
-     * @param Block The object to be registered.
+     * 
+     * @param Block
+     *            The object to be registered.
      */
-    public static void registerBlockWithOreDictionary(SingularityBlock Block) {
-        OreDictionary.registerOre(Block.getName(), Block);
+    public static void registerBlockWithOreDictionary(SingularityBlock Block)
+    {
+	OreDictionary.registerOre(Block.getName(), Block);
     }
-
 
     private static TechbenchRecipeHandler techbenchRecipeHandler = new TechbenchRecipeHandler();
     private static WorkbenchRecipeHandler workbenchRecipeHandler = new WorkbenchRecipeHandler();
@@ -61,25 +67,32 @@ public class SingularityRegistery extends GameRegistry {
 
     /**
      * To register a recipe.
-     * @param commonRecipe The recipe object to be registered.
-     * @return Whether the registration is succeeded (fail if and only if there are conflict recipes).
+     * 
+     * @param commonRecipe
+     *            The recipe object to be registered.
+     * @return Whether the registration is succeeded (fail if and only if there
+     *         are conflict recipes).
      */
-    public static boolean registerRecipe(CommonRecipe commonRecipe) {
-        if (commonRecipe instanceof WorkbenchRecipe)
-            return workbenchRecipeHandler.addRecipe((WorkbenchRecipe) commonRecipe);
-        else if (commonRecipe instanceof TechbenchRecipe)
-            return techbenchRecipeHandler.addRecipe((TechbenchRecipe) commonRecipe);
-        else
-            return commonRecipeHandler.addRecipe(commonRecipe);
+    public static boolean registerRecipe(CommonRecipe commonRecipe)
+    {
+	if (commonRecipe instanceof WorkbenchRecipe)
+	    return workbenchRecipeHandler.addRecipe((WorkbenchRecipe) commonRecipe);
+	else if (commonRecipe instanceof TechbenchRecipe)
+	    return techbenchRecipeHandler.addRecipe((TechbenchRecipe) commonRecipe);
+	else
+	    return commonRecipeHandler.addRecipe(commonRecipe);
     }
 
     /**
      * To get the possible recipe for the input items.
-     * @param itemIn The item used in the recipe.
+     * 
+     * @param itemIn
+     *            The item used in the recipe.
      * @return The recipe object.
      */
-    public static TechbenchRecipe getTechbenchRecipe(ItemStack[] itemIn) {
-        return techbenchRecipeHandler.getRecipe(itemIn);
+    public static TechbenchRecipe getTechbenchRecipe(ItemStack[] itemIn)
+    {
+	return techbenchRecipeHandler.getRecipe(itemIn);
     }
 
 }
--- src/main/java/org/infinitystudio/singularity/api/recipe/CommonRecipe.java
+++ src/main/java/org/infinitystudio/singularity/api/recipe/CommonRecipe.java
@@ -1,30 +1,32 @@
 /**
- *  Singularity Mod for Minecraft.
- *  Copyright (C) 2015 Infinity Studio.
+ * Singularity Mod for Minecraft. Copyright (C) 2015 Infinity Studio.
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *  
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *  @license GPLv3
-*/
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @license GPLv3
+ */
 package org.infinitystudio.singularity.api.recipe;
 
 import net.minecraft.item.ItemStack;
 
 /**
  * Craft recipes.
+ * 
  * @author Lasm_Gratel
  */
-public class CommonRecipe {
+public class CommonRecipe
+{
 
     // 系统分配 合成表唯一ID
     private int id;
@@ -35,33 +37,40 @@ public class CommonRecipe {
 
     protected boolean available;
 
-    public CommonRecipe(ItemStack[] in, ItemStack[] out) {
-        this.in = in;
-        this.out = out;
-        available = true;
+    public CommonRecipe(ItemStack[] in, ItemStack[] out)
+    {
+	this.in = in;
+	this.out = out;
+	available = true;
     }
 
-    public ItemStack[] getOut() {
-        return out;
+    public ItemStack[] getOut()
+    {
+	return out;
     }
 
-    public void setOut(ItemStack[] out) {
-        this.out = out;
+    public void setOut(ItemStack[] out)
+    {
+	this.out = out;
     }
 
-    public ItemStack[] getIn() {
-        return in;
+    public ItemStack[] getIn()
+    {
+	return in;
     }
 
-    public void setIn(ItemStack[] in) {
-        this.in = in;
+    public void setIn(ItemStack[] in)
+    {
+	this.in = in;
     }
 
-    public int getID() {
-        return id;
+    public int getID()
+    {
+	return id;
     }
 
-    public void setID(int ID) {
-        this.id = ID;
+    public void setID(int ID)
+    {
+	this.id = ID;
     }
 }
