219ccf77de24af4a9b84a7b4797d4b132f8170d8
Blealtan <bolee159632@live.com>
2015-11-07 22:18:40 +0800

    Added some javadoc.

--- src/main/java/org/infinitystudio/singularity/api/resource/ResourcePacket.scala
+++ src/main/java/org/infinitystudio/singularity/api/resource/ResourcePacket.scala
@@ -7,8 +7,15 @@ import scala.collection.mutable
  */
 class ResourcePacket(resources: Array[Resource]) {
   private val packet : mutable.Map[Resource.SourceType, Resource] = mutable.Map()
+
+  // Add the resources to packet
   if (resources != null) resources.foreach(r => packet getOrElseUpdate (r.getType, r))
 
+  /**
+   * Add a resource packet to this one.
+   * @param resourcePacket The packet to be added here.
+   * @return A reference to this packet.
+   */
   def +=(resourcePacket: ResourcePacket) : ResourcePacket = {
     resourcePacket.packet.foreach(r => {
       val tmp = packet(r._1)
@@ -18,11 +25,21 @@ class ResourcePacket(resources: Array[Resource]) {
     this
   }
 
+  /**
+   * Add this packet with another one.
+   * @param resourcePacket Another packet to be added.
+   * @return The summation of the two packets.
+   */
   def +(resourcePacket: ResourcePacket) : ResourcePacket = {
     val ret = new ResourcePacket(packet.values.toArray)
     ret += resourcePacket
   }
 
+  /**
+   * Minus this packet with another one.
+   * @param resourcePacket The packet to take calculation.
+   * @return A reference to this packet.
+   */
   def -=(resourcePacket: ResourcePacket) : ResourcePacket = {
     resourcePacket.packet.foreach(r => {
       val tmp = packet(r._1)
@@ -32,12 +49,27 @@ class ResourcePacket(resources: Array[Resource]) {
     this
   }
 
+  /**
+   * Minus this packet with another one.
+   * @param resourcePacket Another packet to minus with.
+   * @return The substitution of the two packets.
+   */
   def -(resourcePacket: ResourcePacket) : ResourcePacket = {
     val ret = new ResourcePacket(packet.values.toArray)
     ret -= resourcePacket
   }
 
-  def get(r: Resource.SourceType) = packet get r
+  /**
+   * Get the resource of some type.
+   * @param sourceType The type of the wanted resource.
+   * @return The wanted resource.
+   */
+  def get(sourceType: Resource.SourceType) = packet get sourceType
 
-  def set(r: Resource.SourceType, res: Resource) = packet update(r, res)
+  /**
+   * Set the resource of some type.
+   * @param sourceType The type of the wanted resource.
+   * @param res The wanted resource.
+   */
+  def set(sourceType: Resource.SourceType, res: Resource) = packet update(sourceType, res)
 }
