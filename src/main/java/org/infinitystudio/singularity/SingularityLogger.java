807ba0d7d599a2beb706241d4dca9749cb0317a7
Darkhighness <729294652hs@gmail.com>
2015-11-07 23:51:02 +0800

    Simple Logger and Research
    Signed-off-by: Darkhighness <729294652hs@gmail.com>

---
+++ src/main/java/org/infinitystudio/singularity/SingularityLogger.java
@@ -0,0 +1,12 @@
+package org.infinitystudio.singularity;
+
+
+
+
+public class SingularityLogger
+{
+    public static void Log(String Message)
+    {
+        System.out.println("[Singularity]" + Message);
+    }
+}
