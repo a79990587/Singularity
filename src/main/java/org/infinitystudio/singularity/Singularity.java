e0f5019faff7b3273cb10fd78f5b3902a19c7f94
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2015-11-08 13:21:41 +0800

    Fix some STUPID bugs made by Lasm

--- src/main/java/org/infinitystudio/singularity/Singularity.java
+++ src/main/java/org/infinitystudio/singularity/Singularity.java
@@ -18,14 +18,8 @@
  */
 package org.infinitystudio.singularity;
 
-
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.network.NetworkRegistry;
-import net.minecraft.item.ItemBlock;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.logging.log4j.Logger;
 import org.infinitystudio.singularity.api.SingularityRegistry;
@@ -35,7 +29,13 @@ import org.infinitystudio.singularity.block.SingularityBlock;
 import org.infinitystudio.singularity.block.SingularityBlockContainer;
 import org.infinitystudio.singularity.block.TechbenchBlock;
 
-import java.util.List;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import net.minecraft.item.ItemBlock;
 
 /**
  * @author Lasm_Gratel Singularity Mod Main.
@@ -45,39 +45,41 @@ public class Singularity {
     public static final String MODID = "singularity";
     public static final String NAME = "Singularity";
     public static final String VERSION = "1.0";
-    
+
     @Instance("Singularity")
     public static Singularity instance;
-    
+
     public static Logger log;
-    
+
     public static List<IBlockBase> blocks;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        log = event.getModLog();
-        
-        log.debug("[Singularity] Add Blocks...");
-        // Add *ALL* Machine Blocks Here.
-        blocks.add(new TechbenchBlock());
-        
-        for (IBlockBase block : blocks) {
-            if (block instanceof IBlockContainerBase) {
-                SingularityBlockContainer b = (SingularityBlockContainer) block;
-                SingularityRegistry.registerBlock(b, ItemBlock.class, b.getName());
-                SingularityRegistry.registerTileEntity(b.getTileEntityClass(), b.getName());
-            } else {
-                SingularityBlock b = (SingularityBlock) block;
-                SingularityRegistry.registerBlock(b, ItemBlock.class, b.getName());
-            }
-        }
-        
-        log.debug("[Singularity] Registring GUI Handler...");
-        NetworkRegistry.INSTANCE.registerGuiHandler(this, new GuiHandler());
+	log = event.getModLog();
+
+	log.debug("[Singularity] Add Blocks...");
+	// Add *ALL* Machine Blocks Here.
+	blocks = new ArrayList<IBlockBase>();
+
+	blocks.add(new TechbenchBlock());
+
+	for (IBlockBase block : blocks) {
+	    if (block instanceof IBlockContainerBase) {
+		SingularityBlockContainer b = (SingularityBlockContainer) block;
+		SingularityRegistry.registerBlock(b, ItemBlock.class, b.getName());
+		SingularityRegistry.registerTileEntity(b.getTileEntityClass(), b.getName());
+	    } else {
+		SingularityBlock b = (SingularityBlock) block;
+		SingularityRegistry.registerBlock(b, ItemBlock.class, b.getName());
+	    }
+	}
+
+	log.debug("[Singularity] Registring GUI Handler...");
+	NetworkRegistry.INSTANCE.registerGuiHandler(this, new GuiHandler());
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-	
+
     }
 }
