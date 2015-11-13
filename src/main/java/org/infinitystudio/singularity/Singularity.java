c38c0430aeb1e47a1a3a419cf0a3f27eafe00e6b
LasmGratel <lasm_gratel@hotmail.com>
2015-11-13 19:50:03 +0800

    add license-header add some licenses format in java

--- src/main/java/org/infinitystudio/singularity/Singularity.java
+++ src/main/java/org/infinitystudio/singularity/Singularity.java
@@ -1,3 +1,22 @@
+/**
+ * Singularity Mod for Minecraft.
+ * Copyright (C) 2015 Infinity Studio.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license GPLv3
+ */
 package org.infinitystudio.singularity;
 
 import org.infinitystudio.singularity.common.CommonProxy;
@@ -10,9 +29,14 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+/**
+ * Singularity Mod Main.
+ *
+ * @author Lasm_Gratel
+ */
 @Mod(modid = Singularity.MODID, name = Singularity.NAME, version = Singularity.VERSION, acceptedMinecraftVersions = "[1.8,)")
-public class Singularity
-{
+public class Singularity {
+
     public static final String MODID = "singularity";
     public static final String NAME = "Singularity";
     public static final String GROUP = "org.infinitystudio.singularity";
@@ -25,20 +49,17 @@ public class Singularity
     public static CommonProxy proxy;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    public void preInit(FMLPreInitializationEvent event) {
         proxy.preInit(event);
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
         proxy.init(event);
     }
 
     @EventHandler
-    public void postInit(FMLPostInitializationEvent event)
-    {
+    public void postInit(FMLPostInitializationEvent event) {
         proxy.postInit(event);
     }
 }
--- src/main/java/org/infinitystudio/singularity/client/ClientProxy.java
+++ src/main/java/org/infinitystudio/singularity/client/ClientProxy.java
@@ -1,3 +1,22 @@
+/**
+ * Singularity Mod for Minecraft.
+ * Copyright (C) 2015 Infinity Studio.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license GPLv3
+ */
 package org.infinitystudio.singularity.client;
 
 import org.infinitystudio.singularity.common.CommonProxy;
@@ -7,27 +26,24 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 /**
- * client proxy on CLIENT SIDE
+ * Client proxy on client side.
  *
  * @author ustc_zzzz
  */
-public class ClientProxy extends CommonProxy
-{
+public class ClientProxy extends CommonProxy {
+
     @Override
-    public void postInit(FMLPostInitializationEvent event)
-    {
+    public void postInit(FMLPostInitializationEvent event) {
         super.postInit(event);
     }
 
     @Override
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
         super.init(event);
     }
 
     @Override
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    public void preInit(FMLPreInitializationEvent event) {
         super.preInit(event);
     }
 }
